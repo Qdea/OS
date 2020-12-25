fe8a9e54832e5d95f0ae2e563dd1c7ca58ef4a27
Qdea <58660439+Qdea@users.noreply.github.com>
2020-12-26 01:47:48 +0300

    Update copy2.bat

--- copy2.bat
+++ copy2.bat
@@ -1,6 +1,2 @@
-1 вариант(копирует файлы из каталога windows без вложенных подпапок(так скорее всего и нужно по заданию)):
 @echo off
-forfiles /P "C:\Windows" /C "cmd /c if @fsize GEQ 2097152 copy @path C:\IEWIN7\Temp /Z /Y" 
-2 вариант(доработка с рекурсивным копированием файлов из подкаталогов):
-@echo off
-forfiles /P "C:\Windows" /S /C "cmd /c if @fsize GEQ 2097152 copy @path C:\IEWIN7\Temp /Z /Y" 
+forfiles /P "C:\Windows" /C "cmd /c if @fsize GEQ 2097152 copy @path C:\IEWIN7\Temp /Z /Y"
